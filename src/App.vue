6980ecdd15f3ca8f08c3ccebdafa4936becb0e5c
luohuidong <luohuidong01@126.com>
2021-04-05 19:02:10 +0800

    feat: 配置 vue router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
-  <img
-    alt="Vue logo"
-    src="./assets/logo.png"
-  >
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <h1>Hello App!</h1>
+  <p>
+    <!-- use the router-link component for navigation. -->
+    <!-- specify the link by passing the `to` prop. -->
+    <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
+    <router-link to="/">
+      Go to Home
+    </router-link>
+
+    <router-link to="/about">
+      Go to About
+    </router-link>
+  </p>
+  <!-- route outlet -->
+  <!-- component matched by the route will render here -->
+  <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    HelloWorld,
-  },
 });
 </script>
 
