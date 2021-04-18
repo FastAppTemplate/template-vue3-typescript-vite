a1d63f7bac1d3f7319d18911d3de1123a2d43128
luohuidong <luohuidong01@126.com>
2021-04-18 15:09:33 +0800

    refactor: 抽取 layout 组件

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,22 @@
 <template>
-  <h1>Hello App!</h1>
-  <p>
-    <!-- use the router-link component for navigation. -->
-    <!-- specify the link by passing the `to` prop. -->
-    <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
-    <router-link to="/">
-      Go to Home
-    </router-link>
-
-    <router-link to="/about">
-      Go to About
-    </router-link>
-  </p>
-  <!-- route outlet -->
-  <!-- component matched by the route will render here -->
-  <router-view />
+  <PageLayout>
+    <router-view />
+  </PageLayout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import PageLayout from "./components/PageLayout/index.vue";
 
 export default defineComponent({
   name: "App",
+  components: {
+    PageLayout,
+  },
 });
 </script>
 
-<style>
+  <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
